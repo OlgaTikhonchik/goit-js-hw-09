a770443926fd872ece2d5a41a0cc6aab59ab73d1
OlgaTikhonchik <olga882011@gmail.com>
2023-03-16 15:01:39 +0200

    Сделано color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+let intervalId = null;
+
+btnStart.addEventListener('click', onClickBtnStart);
+
+btnStop.addEventListener('click', onClickBtnStop);
+btnStop.disabled = true;
+
+function onClickBtnStart() {
+  intervalId = setInterval(bodyColorChange, 1000);
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+}
+
+function onClickBtnStop() {
+  clearInterval(intervalId);
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+}
+
+function bodyColorChange() {
+  let color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+}
