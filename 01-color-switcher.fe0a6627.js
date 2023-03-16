2246cb69e3fadebde938b3a6cf83820d447ace3c
OlgaTikhonchik <114959595+OlgaTikhonchik@users.noreply.github.com>
2023-03-16 13:02:07 +0000

    Deploying to gh-pages from @ OlgaTikhonchik/goit-js-hw-09@a770443926fd872ece2d5a41a0cc6aab59ab73d1 🚀

---
+++ 01-color-switcher.fe0a6627.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;function a(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.backgroundColor=t}t.addEventListener("click",(function(){n=setInterval(a,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0})),e.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.fe0a6627.js.map
