5b29341ca8dc397fb80c699c3378b8a97819cf4b
OlgaTikhonchik <olga882011@gmail.com>
2023-03-17 23:13:50 +0200

    Завершение задания с промисами

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSummitForm);
+
+function onSummitForm(event) {
+  event.preventDefault();
+
+  const amount = parseInt(form.amount.value);
+  const step = parseInt(form.step.value);
+  const delay = parseInt(form.delay.value);
+
+  for (let i = 0; i < amount; i += 1) {
+    let position = i + 1;
+
+    createPromise(position, delay + i * step)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
+  event.currentTarget.reset();
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
